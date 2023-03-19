9075794cfb062578f14cb495687a9327cec24309
BYC781 <benvv1234@gmail.com>
2023-03-19 22:31:53 +0800

    assign 1 done

--- assign1/Function.R
+++ assign1/Function.R
@@ -7,7 +7,6 @@ get_coef <- function(method, beta, model, dgp){
     else if (method == "Ridge"){
         ridge_model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
                                  alpha = 0,
-                                 lambda = lambdas,
                                  nfolds =10)
         ridge_best <- glmnet(X, y, alpha = 0,lambda = ridge_model$lambda.min)
         return(coef(ridge_best)[beta+1])
@@ -15,37 +14,40 @@ get_coef <- function(method, beta, model, dgp){
     else if(method == "LASSO"){
         lasso_model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
                                  alpha = 1,
-                                 lambda = lambdas,
                                  nfolds =10)
-        lasso_best <- glmnet(X, y, alpha = 1,lambda = ridge_model$lambda.min)
+        lasso_best <- glmnet(X, y, alpha = 1,lambda = lasso_model$lambda.min)
         return(coef(lasso_best)[beta+1])
     }
 }
 
-get_pred <- function(method, model, dgp){
+get_pred_minus_test <- function(method, model, dgp){
     if (model == 1) X <- X[,1:26]
     if (dgp == 1) y <- y_dgp1 else y <- y_dgp2
     if (method == "OLS"){
         train_df <- cbind(data.frame(Y=y[tra.idx,]), data.frame(X[tra.idx,]))
         new <- data.frame(X[-tra.idx,])
         my_lm <- lm(Y~., data=train_df)
-        return(predict(my_lm, new))
-    }
-    else if (method == "Ridge"){
-        model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
+        return(predict(my_lm, new) - y[-tra.idx,])
+        
+    }else if (method == "Ridge"){
+        ridge_model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
                            alpha = 0,
-                           lambda = lambdas,
                            nfolds =10)
-        best <- glmnet(X[tra.idx,], y[tra.idx,], alpha = 0,lambda = model$lambda.min)
-        return(predict.glmnet(best, X[-tra.idx,]))
-    }
-    else if(method == "LASSO"){
-        model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
+        
+        ridge_best <- glmnet(X[tra.idx,], y[tra.idx,], alpha = 0,
+                             lambda = ridge_model$lambda.min)
+        
+        return(predict.glmnet(ridge_best, X[-tra.idx,]) - y[-tra.idx,])
+        
+    }else if(method == "LASSO"){
+        lasso_model <- cv.glmnet(X[tra.idx, ],y[tra.idx, ],
                            alpha = 1,
-                           lambda = lambdas,
                            nfolds =10)
-        best <- glmnet(X[tra.idx,], y[tra.idx,], alpha = 1,lambda = model$lambda.min)
-        return(predict.glmnet(best, X[-tra.idx,]))
+        
+        lasso_best <- glmnet(X[tra.idx,], y[tra.idx,], alpha = 1,
+                             lambda = lasso_model$lambda.min)
+        
+        return(predict.glmnet(lasso_best, X[-tra.idx,]) - y[-tra.idx,])
     }
 }
 
@@ -63,7 +65,7 @@ draw_and_get <- function(method, beta, model, dgp){
     get_coef(method, beta, model, dgp)
 }
 
-draw_and_pred <- function(method, model, dgp){
+draw_and_pred_minus_test <- function(method, model, dgp){
     drawn()
-    get_pred(method, model, dgp)
+    get_pred_minus_test(method, model, dgp)
 }
--- assign1/assign1.R
+++ assign1/assign1.R
@@ -3,39 +3,62 @@ library(dplyr)
 source("Function.R")
 beta_vector <- rep(1, 21)
 beta <- setNames(beta_vector, paste0("b", 0:20))
-n <- 500; p <- 50; tra.idx <- 1:400; R <- 2000
-lambdas <- seq(0,2, length.out = 100)
+n <- 500; p <- 50; tra.idx <- 1:400; R <- 20
 
 
 
 # main1: beta_method_dgp_model
 set.seed(1234)
 container <- matrix(nrow=R, ncol=24)
-params <- expand.grid(b_hat=c(1, 21), met=c("OLS", "Ridge", "LASSO"), dt=1:2, md=1:2)
+params <- expand.grid(b_hat=c(1, 21), 
+                      met=c("OLS", "Ridge", "LASSO"), dt=1:2, md=1:2)
 colnames(container) <- apply(params, 1, paste0, collapse="_")
 container[] <- mapply(function(b_hat, met, dt, md) {
-    sapply(1:R, function(...) draw_and_get(method=met, beta=b_hat, model=md, dgp=dt))
+    sapply(1:R, function(...) 
+        draw_and_get(method=met, beta=b_hat, model=md, dgp=dt))
 }, params$b_hat,params$met,params$dt,params$md)
+
 
 
 
 # main2: method_dgp_model
 set.seed(1234)
-R <- 10
 params2 <- expand.grid(met=c("OLS", "Ridge", "LASSO"), dt=1:2, md=1:2)
-names(matrix_list) <- apply(params2, 1, paste0, collapse="_")
+matrix_list <- lapply(1:12, function(x) matrix(nrow = 100, ncol = R))
 matrix_list <- lapply(seq_along(matrix_list), function(i) {
-    sapply(1:R, function(r) {
-        draw_and_pred(met=params2[i,"met"], model=params2[i,"md"], dgp=params2[i,"dt"])
+    sapply(1:R, function(r){
+        draw_and_pred_minus_test(met=params2[i,"met"], 
+                                 model=params2[i,"md"], 
+                                 dgp=params2[i,"dt"])
     })
 })
-matrix_list <- lapply(matrix_list, t)
 names(matrix_list) <- apply(params2, 1, paste0, collapse="_")
 
 
 
 
-col_means <- colMeans(container)
-col_sds <- apply(container, 2, var) %>% sqrt()
-col_rmses.for.b1 <- apply(container[,seq(1,24,2)], 2, function(col) sqrt((1/length(col)) * sum((col - 1)^2)))
-col_rmses.for.b21 <- apply(container[,seq(2,24,2)], 2, function(col) sqrt((1/length(col)) * sum((col - 0)^2)))
+##a
+mean.for.b1 <- apply(container[,seq(1,24,2)], 2, mean)
+sd.for.b1 <- apply(container[,seq(1,24,2)], 2, var) %>% sqrt()
+rmses.for.b1 <- apply(container[,seq(1,24,2)], 2, 
+                      function(col) sqrt((1/length(col)) * sum((col - 1)^2)))
+
+##b
+mean.for.b21 <- apply(container[,seq(2,24,2)], 2, mean)
+sd.for.b21 <- apply(container[,seq(2,24,2)], 2, var) %>% sqrt()
+rmses.for.b21 <- apply(container[,seq(2,24,2)], 2, 
+                       function(col) sqrt((1/length(col)) * sum((col - 0)^2)))
+
+##c
+MSPE_method_dgp_model <- numeric(12)
+for (i in 1:12){
+    sum <- 0
+    for (col in 1:R){
+        sum <- sum + 0.01*t(matrix_list[[i]][,col]) %*% matrix_list[[i]][,col]
+    }
+    result <- R^-1 * sum
+    print(result)
+    MSPE_method_dgp_model[i] <- result
+}
+names(MSPE_method_dgp_model) <- apply(params2, 1, paste0, collapse="_")
+MSPE_method_dgp_model
